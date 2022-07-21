f2c917e07704f6fd96ecb34cd7ca376ca7a7c2ff
Nuno Moniz <nuno-moniz_student2022@wilder.school>
2022-07-21 15:24:19 +0100

    Added about text

--- project/src/components/about/about.jsx
+++ project/src/components/about/about.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 export default function About() {
-    
+
     return (
         <div>
         <h1>About</h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            quidem, quisquam.
-        </p>
+            <p>
+                Im a 22-years-old junior web developer based in Lisbon.
+                Over the past 6 years, I have been exploring and discovering my passions, from video and photography to web development. Always with the main objective of making a good impact in our society and being able to improve professionally and personally.
+                In a permanent pursuit of ways to innovate and solve problems to achieve my objectives and complete my work. Constantly eager to improve my craft and expand my knowledge.
+            </p>
         </div>
     );
 }
